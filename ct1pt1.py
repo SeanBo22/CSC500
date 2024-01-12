7aa95b13f9b7d4f8c21a3df3cb0ee10fd6b38784
Sean Bohuslavsky <sean.bohuslavsky@csuglobal.edu>
2024-01-12 15:06:35 -0700

    init

---
+++ ct1pt1.py
@@ -0,0 +1,21 @@
+## Sean Bohuslavsky 
+## CSC500 - Critical Thinking Assignment 1, Part 1
+## Python Prorgam to add and subtract two numbers
+
+# Add two numbers and print result
+def add(num1, num2):
+    sum = num1 + num2
+    print("Sum: {}".format(sum))
+
+# Subtract two numbers and print result
+def sub(num1, num2):
+    dif = num1 - num2
+    print("Difference: {}".format(dif))
+
+# Get user input
+num1 = float(input("Enter number 1: "))
+num2 = float(input("Enter number 2: "))
+
+# Call functions
+add(num1, num2)
+sub(num1, num2)
---
+++ ct1pt2.py
@@ -0,0 +1,21 @@
+## Sean Bohuslavsky 
+## CSC500 - Critical Thinking Assignment 1, Part 2
+## Python Prorgam to multiply and divide two numbers
+
+# Multiply two numbers and print result
+def mul(num1, num2):
+    prod = num1 * num2
+    print("Product: {}".format(prod))
+
+# Divide two numbers and print result
+def div(num1, num2):
+    quot = num1 / num2
+    print("Quotient: {}".format(quot))
+
+# Get user input
+num1 = float(input("Enter number 1: "))
+num2 = float(input("Enter number 2: "))
+
+# Call functions
+mul(num1, num2)
+div(num1, num2)
