ba3f8436f9c92b500b35eff20b11717edfeef24f
Sean Bohuslavsky <sean.bohuslavsky@csuglobal.edu>
2024-01-31 16:58:22 -0700

    spelling

--- ct3pt2.py
+++ ct3pt2.py
@@ -1,6 +1,6 @@
 ## Sean Bohuslavsky 
 ## CSC500 - Critical Thinking Assignment 3, Part 2
-## Python Program to caclulate when alram clock with go off
+## Python Program to calculate when alarm clock with go off
 
 # Function to print hour an alarm will go off given current time and time between
 def alarm(current_time, time_between):
